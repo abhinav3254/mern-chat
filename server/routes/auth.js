30778c26bf87b010f1e73eb5cd8bbcbecc9b6e0d
abhinav3254 <abhinav3254@gmail.com>
2024-04-10 23:47:18 +0530

    login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 
 router.get('/', (req, res) => {
@@ -11,9 +14,21 @@ router.get('/', (req, res) => {
 /**
  * This method is for login of user
  */
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
 	const { email, password } = req.body;
-	res.json(req.body);
+	try {
+		const userDetails = await User.findOne({ email: email });
+		if (!userDetails) {
+			return res.status(404).json({ message: 'user not found' });
+		}
+		const isPasswordSame = await comparePassword(password, userDetails.password);
+		if (isPasswordSame) {
+			return res.status(200).json({ message: 'ok' });
+		}
+		return res.status(400).json({ message: 'wrong password' });
+	} catch (err) {
+		return res.status(500).json({ message: 'internal server error' });
+	}
 });
 
 /**
@@ -21,12 +36,39 @@ router.post('/login', (req, res) => {
  */
 router.post('/register', async (req, res) => {
 	const { name, email, password } = req.body;
-	const newUser = new User({ name, email, password });
-	const databaseResponse = await newUser.save();
-	console.log(databaseResponse);
-	res.json(req.body);
+	try {
+		// checking if all the fields are there in request body or not
+		if (!email || !password || !name) {
+			return res.status(404).json({ message: 'all fields are mandatory' });
+		}
+
+		// checking if user already exists in database with this name
+		const existingUser = await User.findOne({ email: email });
+		if (existingUser) {
+			return res.status(409).json({ message: 'Email already in use' });
+		}
+		// hashing password
+		const hashPassword = await passwordHashing(password);
+		const newUser = new User({ name, email, password: hashPassword });
+		const databaseResponse = await newUser.save();
+		return res.status(200).json({ message: 'Registration successful' })
+	} catch (err) {
+		return res.status(500).json({ message: 'something went wrong!' })
+	}
 });
 
+/**
+ * This method will hash the plain password
+ */
+async function passwordHashing(plainPassword) {
+	const hash = await bcrypt.hash(plainPassword, saltRounds);
+	return hash;
+}
+
+async function comparePassword(plainPassword, hashedPassword) {
+	const match = await bcrypt.compare(plainPassword, hashedPassword);
+	return match;
+}
 
 
 module.exports = router;
